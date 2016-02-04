621ae8f3529054a0040b418be66ff94949a8acbd
Yash Dalal <yashdalal95@gmail.com>
2016-02-04 12:00:06 -0600

    Initial Commit

---
+++ ZapposCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZapposCalculator
+//
+//  Created by Yash Dalal on 2/4/16.
+//  Copyright © 2016 Yash Dalal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
